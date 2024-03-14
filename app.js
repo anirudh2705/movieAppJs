7befc1283eca55dc611dabead05f83d0912dfd01
Anirudh <anirudhpatnaik2804@gmail.com>
2024-03-14 15:10:50 +0530

    bug fixes

--- app.js
+++ app.js
@@ -8,11 +8,11 @@ let ratings = 0;
 let genre = "";
 let filteredMovies = [];
 
-const URL = "https://movies-app.prakashsakari.repl.co/api/movies";
+const URL = "https://replit.com/AnirudhPatnaik1/movie-api";
 
-const getMovies = async (url) => {
+const getMovies = async (URL) => {
   try {
-    const { data } = await axios.get(url); // axios converts the data into jason on its own
+    const { data } = await axios.get(URL); // axios converts the data into jason on its own
     return data;
   } catch (err) {}
 };
@@ -26,11 +26,11 @@ const createElement = (element) => document.createElement(element);
 const createMoviecard = (movies) => {
   for (let movie of movies) {
     // creating parent container
-    const cardContainer = createElement(div);
+    const cardContainer = createElement("div");
     cardContainer.classList.add("card", "shadow");
 
     //creating image container
-    const imageContaier = cardContainer.appendChild(createElement(div));
+    const imageContaier = cardContainer.appendChild(createElement("div"));
     imageContaier.classList.add("card-image-container");
     const img = imageContaier.appendChild(createElement("img"));
     img.classList.add("card-image");
@@ -60,16 +60,16 @@ const createMoviecard = (movies) => {
     ratings.classList.add("star-rating");
 
     //star icon
-    const icon = star.appendChild(createElement("span"));
+    const icon = ratings.appendChild(createElement("span"));
     icon.classList.add("material-symbols-outlined");
     icon.innerHTML = "star";
 
     //ratings
-    const ratingVal = star.appendChild("span");
+    const ratingVal = ratings.appendChild(createElement("span"));
     ratingVal.innerHTML = movie.imdb_rating;
 
     // movie duration
-    const duration = ratingContainer.appendChild("p");
+    const duration = ratingContainer.appendChild(createElement("p"));
     duration.innerHTML = `${movie.duration} mins`;
 
     parentContainer.appendChild(cardContainer);
@@ -133,7 +133,7 @@ function handleRatingSelect(event){
 
 movieRatings.addEventListener("change", handleRatingSelect);
 
-const genres = movie.reduce((acc,cur) => {
+const genres = movies.reduce((acc,cur) => {
     let genresArr = [];
     let tempGenresArr = cur.genre.split(",");
     acc = [...acc, ...tempGenresArr];
@@ -160,7 +160,5 @@ function handleGenreSelect(event){
     createMoviecard(genre ? filterByGenre : genre);
 }
 
-movieGenres.addEventListener("change", handleGenreSelect){
-
-}
+movieGenres.addEventListener("change", handleGenreSelect);
 // shift alt f
